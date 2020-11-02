d72d44c9df275f3fc8b429d8fcf23b1934075e68
JungwooSim <cscd053@gmail.com>
2020-11-03 08:52:11 +0900

    배치에 필요한 스키마 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `big_local` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `big_local` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `small_local` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `small_local` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `tmp_library` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `big_local` varchar(45) DEFAULT NULL,
+  `library_nm` varchar(45) DEFAULT NULL,
+  `library_type` varchar(45) DEFAULT NULL,
+  `small_local` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13273 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `library` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `library_nm` int(11) DEFAULT NULL,
+  `big_local_id` int(11) DEFAULT NULL,
+  `libaray_type_id` int(11) DEFAULT NULL,
+  `small_local_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
